f9c0ab3f80411ecf62364e4d56a37d9f3c07c545
MoonsterXD2000 <alvaro.ciudad5@educa.madrid.org>
2024-12-13 19:18:00 +0100

    Add files via upload
    mi primera linea de codigo :DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // tenemos que ponerlo abajo de la linea start entre el corchete poner print parentesis y las 2  comillas poner hello ''tu nombre''y cerrar la comilla el parentesis y no te olvides de poner el punto y coma que te veo -_-
+        print("hello patatita");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
